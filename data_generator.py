3888ab26643978262dace4b13ccd3109783fc35d
jrbostic <jesse.r.bostic@gmail.com>
2014-10-24 19:36:27 -0700

    Completed data generator should work now.  Additional items can simply be appended as literals into the db_values dict (in appropriate list).  Added db creation/reset .sql file.

--- data_generator.py
+++ data_generator.py
@@ -3,13 +3,96 @@ import random
 
 __author__ = 'jessebostic'
 
-db = MySQLdb.connect(host = "50.62.209.116", port=3306, user="TCSS_445", passwd="L3mm31N!", db="Snootboots")
+# Holds all of the possibly string values for creating random items
+# Integers should be dynamically created with random module upon insertion
+db_values = {"name_adjectives": ["Ugly", "Cheap", "Elegant", "Unique", "Used", "Gorgeous", "Plain", "Barbed", "Smooth"],
+             "shapes": ["Sphere", "Triangle", "Tight Fit", "Square"],
+             "types": ["Butterfly", "Button", "Snap", "Screw On"],
+             "materials": ["Wool", "Polyester", "Plastic", "Bronze", "Sterling Silver", "Glass"],
+             "colors": ["Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Translucent"],
+             "boot_mfgs": ["Boot The World", "NoseStyl", "Shnoot", "Bootcetera"],
+             "tie_mfgs": ["ThredLite", "RevoSnootion", "OGG", "Spinweel"],
+             "clasp_mfgs": ["YKK", "Claspsmith and Sons", "Lockitup", "Stevenson & Kline"],
+             "mfgs_cities": [("Tacoma", "WA"), ("San Francisco", "CA"), ("New York", "NY"), ("Salt Lake City", "UT"),
+                             ("Miami", "FL"), ("Dallas", "TX"), ("Tucson", "AZ"), ("St. Paul", "MN")]}
 
+# How many items of each type to generate.
+NUM_OF_ITEMS = 15
+
+db = MySQLdb.connect(host="50.62.209.116", port=3306, user="TCSS_445", passwd="L3mm31N!", db="Snootboots")
 cur = db.cursor()
 
-#cur.execute("INSERT INTO SNOOTBOOTS (Name) VALUES ('A Special Boot')")
+#INSERT MFGS
+for name in db_values["boot_mfgs"]:
+    loc = random.choice(db_values["mfgs_cities"])
+    cur.execute('INSERT INTO MFGS (Name, ProductType, City, State) '
+                'VALUES ("{}", "{}", "{}", "{}")'.format(name, "Boots", loc[0], loc[1]))
 
-cur.execute("SELECT * FROM SNOOTBOOTS")
+for name in db_values["tie_mfgs"]:
+    loc = random.choice(db_values["mfgs_cities"])
+    cur.execute('INSERT INTO MFGS (Name, ProductType, City, State) '
+                'VALUES ("{}", "{}", "{}", "{}")'.format(name, "Ties", loc[0], loc[1]))
 
-for row in cur.fetchall():
-    print row
+for name in db_values["clasp_mfgs"]:
+    loc = random.choice(db_values["mfgs_cities"])
+    cur.execute('INSERT INTO MFGS (Name, ProductType, City, State) '
+                'VALUES ("{}", "{}", "{}", "{}")'.format(name, "Clasps", loc[0], loc[1]))
+
+#INSERT BOOTS
+cur.execute('SELECT ID FROM MFGS WHERE ProductType = "Boots"')
+mfg_ids = cur.fetchall()
+for x in range(NUM_OF_ITEMS):
+    name = random.choice(db_values["name_adjectives"]) + ' ' + random.choice(db_values["name_adjectives"]) + ' ' + "Boot"
+    shape =  random.choice(db_values["shapes"])
+    material = random.choice(db_values["materials"])
+    color = random.choice(db_values["colors"])
+    mfg = random.choice(mfg_ids)[0]
+    price = random.randint(1, 100) + random.random() + random.random() * 0.1
+    cur.execute('INSERT INTO BOOTS (Name, Shape, Material, Color, Mfg, Price) '
+                'VALUES ("{}", "{}", "{}", "{}", {}, {})'.format(name, shape, material, color, mfg, price))
+
+#INSERT TIES
+cur.execute('SELECT ID FROM MFGS WHERE ProductType = "Ties"')
+mfg_ids = cur.fetchall()
+for x in range(NUM_OF_ITEMS):
+    name = random.choice(db_values["name_adjectives"]) + ' ' + random.choice(db_values["name_adjectives"]) + ' ' + "Boot"
+    length =  random.randint(8, 25)
+    material = random.choice(db_values["materials"])
+    color = random.choice(db_values["colors"])
+    mfg = random.choice(mfg_ids)[0]
+    price = random.randint(1, 100) + random.random() + random.random() * 0.1
+    cur.execute('INSERT INTO TIES (Name, Length, Material, Color, Mfg, Price) '
+                'VALUES ("{}", "{}", "{}", "{}", {}, {})'.format(name, length, material, color, mfg, price))
+
+#INSERT CLASPS
+cur.execute('SELECT ID FROM MFGS WHERE ProductType = "Clasps"')
+mfg_ids = cur.fetchall()
+for x in range(NUM_OF_ITEMS):
+    name = random.choice(db_values["name_adjectives"]) + ' ' + random.choice(db_values["name_adjectives"]) + ' ' + "Boot"
+    type =  random.choice(db_values["types"])
+    material = random.choice(db_values["materials"])
+    color = random.choice(db_values["colors"])
+    mfg = random.choice(mfg_ids)[0]
+    price = random.randint(1, 100) + random.random() + random.random() * 0.1
+    cur.execute('INSERT INTO CLASPS (Name, Type, Material, Color, Mfg, Price) '
+                'VALUES ("{}", "{}", "{}", "{}", {}, {})'.format(name, type, material, color, mfg, price))
+
+cur.close()
+db.close()
+
+
+
+#OPEN CONNECTION AND CURSOR
+# db = MySQLdb.connect(host="50.62.209.116", port=3306, user="TCSS_445", passwd="L3mm31N!", db="Snootboots")
+# cur = db.cursor()
+
+# cur.execute("INSERT INTO SNOOTBOOTS (Name) VALUES ('A Special Boot')")
+#
+# cur.execute("SELECT * FROM SNOOTBOOTS")
+#
+# for row in cur.fetchall():
+#     print row
+
+#CLOSE CONNECTION AND CURSOR
+# cur.close()
+# db.close()

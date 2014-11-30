256b7c0dce5bc6ea2afccdb19609cfc760d35587
Nick Ames <names144@gmail.com>
2014-11-29 20:25:43 -0800

    Find function works now. Can query DB with any fields

--- query_library.py
+++ query_library.py
@@ -14,7 +14,6 @@ def close_conn(db, cur):
     db.close()
     cur.close()
 
-
 def connection(query_func):
     """Decorator takes query function and returns result."""
 
@@ -29,6 +28,139 @@ def connection(query_func):
         return result
 
     return doit
+
+def action(name, comp_list):
+    """Finds what button was pressed and queries based on boot, tie or clasp"""
+
+    def valid_price(p):
+        """Tests if a price is a valid number"""
+        try:
+            float(p)
+            return True
+        except ValueError:
+            return False
+
+    query = "SELECT * FROM (" + name.upper() + "S" + " JOIN MFGS ON Mfg = MFGS.ID)"
+    # Name entry
+    text = comp_list[0].get()
+    if text == "Name" or text == "":
+        text = ""
+    else:
+        # Two cases, names containing text OR names == text
+        text = name.upper() + "S.Name LIKE " + "'%" + text + "%'"
+        #text = name.upper() + "S.Name=" + "'" + text + "'"
+
+    # Dropdown values
+    drop1 = comp_list[1].cget("text")
+    if drop1 != "Shape" and drop1 != "ALL" and name == "Boot":
+        drop1 = "Shape=" + "'" + drop1 + "'"
+    elif drop1 != "Length" and drop1 != "ALL" and name == "Tie":
+        drop1 = "Length=" + "'" + drop1 + "'"
+    elif drop1 != "Type" and drop1 != "ALL" and name == "Clasp":
+        drop1 = "Type=" + "'" + drop1 + "'"
+    else:
+        drop1 = ""
+
+    drop2 = comp_list[2].cget("text")
+    if drop2 != "Material" and drop2 != "ALL":
+        drop2 = "Material=" + "'" + drop2 + "'"
+    else:
+        drop2 = ""
+
+    drop3 = comp_list[3].cget("text")
+    if drop3 != "Color" and drop3 != "ALL":
+        drop3 = "Color=" + "'" + drop3 + "'"
+    else:
+        drop3 = ""
+
+    drop4 = comp_list[4].cget("text")
+    if drop4 != "Manufacturer" and drop4 != "ALL":
+        drop4 = "MFGS.Name=" + "'" + drop4 + "'"
+    else:
+        drop4 = ""
+
+    # prices
+    min = comp_list[5].get()
+    max = comp_list[6].get()
+
+    if min == "Min Price" or min == "":
+        min = ""
+    else:
+        if valid_price(min):
+            min = "Price>" + min
+        else:
+            min = "Invalid input for min price."
+
+    if max == "Max Price" or max == "":
+        max = ""
+    else:
+        if valid_price(max):
+            max = "Price<" + max
+        else:
+            max = "Invalid input for max price."
+
+    # build the query statement, check for no/bad inputs
+    if text != "" or drop1 != "" or drop2 != "" or drop3 != "" or drop4 != "" or min != "" or max != "":
+        query += " WHERE "
+    else:
+        #query db here for ANY
+        print(query)
+        res = find(query)
+        print(res)
+        return
+
+    # build the statement
+    andit = False
+    if text != "":
+        query += text + " "
+        andit = True
+    if drop1 != "":
+        if andit:
+            query += "AND "
+        query += drop1 + " "
+        andit = True
+    if drop2 != "":
+        if andit:
+            query += "AND "
+        query += drop2 + " "
+        andit = True
+    if drop3 != "":
+        if andit:
+            query += "AND "
+        query += drop3 + " "
+        andit = True
+    if drop4 != "":
+        if andit:
+            query += "AND "
+        query += drop4 + " "
+        andit = True
+    if min != "":
+        if min != "Invalid input for min price.":
+            if andit:
+                query += "AND "
+            query += min + " "
+            andit = True
+        else:
+            # error here don't query
+            print(min)
+            return
+    if max != "":
+        if max != "Invalid input for max price.":
+            if andit:
+                query += "AND "
+            query += max + " "
+        else:
+            # error here don't query
+            print(max)
+            return
+    print(query)
+    res = find(query)
+    print(res)
+    return
+
+@connection
+def find(statement):
+    return statement
 
 @connection
 def get_boot_shapes():
--- snootboot_gui.py
+++ snootboot_gui.py
@@ -42,11 +42,13 @@ class SnootbootGUI:
 
             Label(self.frame, text="Find A "+comp_list[-1].title()+": ").grid(row=row, column=0)
 
+            # Input Label for Name
             text = comp_list[0]
             comp_list[0] = Entry(self.frame)
             comp_list[0].insert(0, text.title())
             comp_list[0].grid(row=row, column=1, sticky=N+S+E+W)
 
+            # Dropdowns for Shape, Length, Type
             text = comp_list[1][0]
             options = [list(i)[0] for i in sorted(comp_list[1][1]())]
             options[:0] = [any]
@@ -58,6 +60,7 @@ class SnootbootGUI:
             # comp_list[1].image=arrow_image
             # print comp_list[1]
 
+            # Dropdowns for Material
             text = comp_list[2]
             options = [list(i)[0] for i in sorted(query_library.get_materials(comp_list[-1]))]
             options[:0] = [any]
@@ -66,6 +69,7 @@ class SnootbootGUI:
             comp_list[2] = apply(OptionMenu, (self.frame, variable) + tuple(options))
             comp_list[2].grid(row=row, column=3, sticky=N+S+E+W)
 
+            # Dropdowns for Color
             text = comp_list[3]
             options = [list(i)[0] for i in sorted(query_library.get_colors(comp_list[-1]))]
             options[:0] = [any]
@@ -76,6 +80,7 @@ class SnootbootGUI:
 
             row += 1
 
+            # Dropdowns for MFG
             text = comp_list[4]
             options = [list(i)[0] for i in sorted(query_library.get_mfgs(comp_list[-1]))]
             options[:0] = [any]
@@ -84,18 +89,22 @@ class SnootbootGUI:
             comp_list[4] = apply(OptionMenu, (self.frame, variable) + tuple(options))
             comp_list[4].grid(row=row, column=1, sticky=N+S+E+W)
 
+            # Input Label for Min Price
             text = comp_list[5]
             comp_list[5] = Entry(self.frame)
             comp_list[5].insert(0, text.title())
             comp_list[5].grid(row=row, column=2, sticky=N+S+E+W)
 
+            # Input Label for Max Price
             text = comp_list[6]
             comp_list[6] = Entry(self.frame)
             comp_list[6].insert(0, text.title())
             comp_list[6].grid(row=row, column=3, sticky=N+S+E+W)
 
+            # Find Button
             text = comp_list[7]
-            comp_list[7] = Button(self.frame, text=text.title())
+            comp_list[7] = Button(self.frame, text=text.title(),
+                                  command=lambda name=comp_list[-1].title(),
+                                                 clist=comp_list: query_library.action(name, clist))
             comp_list[7].grid(row=row, column=4, sticky=N+S+E+W)
-
             row += 1
